0951fffe721175d8d3223596bc6e3fd7bd6c54cf
trapano-monogamo <filippo.cozzi.fc@gmail.com>
2024-03-31 17:04:25 +0200

    shading the hell out of these cubes, innit

--- build/engine/CMakeFiles/rendering_engine.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "librendering_engine.a"
-)
--- build/engine/CMakeFiles/rendering_engine.dir/src/math/utils.cpp.o.d
+++
@@ -1,4 +0,0 @@
-engine/CMakeFiles/rendering_engine.dir/src/math/utils.cpp.o: \
- /home/chiara/dev/cpp/rendering_engine/engine/src/math/utils.cpp \
- /usr/include/stdc-predef.h \
- /home/chiara/dev/cpp/rendering_engine/engine/include/math/utils.hpp
